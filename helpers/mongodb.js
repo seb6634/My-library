3cfd2e75d2534d0132f3da3448c057bf28ad4b22
sebastien gonzalez <gonzalez.sebastien66@gmail.com>
2021-09-23 19:20:41 +0200

    new config to connect db

--- helpers/mongodb.js
+++ helpers/mongodb.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://user61489636:AnMdH9i8SpDOAUi0@cluster0.qfasi.mongodb.net/library?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfasi.mongodb.net/library?retryWrites=true&w=majority`
   );
 
   return client;
